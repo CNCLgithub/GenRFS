a54635d3740790c435f98844ee3d3a1c766e5ac9
belledon <mbelledonne@gmail.com>
2020-07-28 17:41:07 -0400

    first pass on partition table

--- src/utils.jl
+++ src/utils.jl
@@ -1,12 +1,20 @@
 export normalize_weights, partition_press
 
 using Base:tail
-using Base.Iterators:product
+using Base.Iterators:product, flatten, rest
+using Memoization
 
 function normalize_weights(log_weights::Vector{Float64})
     log_total_weight = logsumexp(log_weights)
     log_normalized_weights = log_weights .- log_total_weight
     return (log_total_weight, log_normalized_weights)
+end
+
+@memoize function partition_table(m::Vector{Int}, k::Int)
+    m = sort(m, rev = true)
+    pressed = partition_press(m, k)
+    rng = collect(1:k)
+    vcat(map(x -> partition_push(x, rng), pressed)...)
 end
 
 function partition_press(m::Vector{Int}, k::Int)
@@ -21,7 +29,6 @@ function partition_press(m::Vector{Int}, k::Int)
     end
     combs = vcat(filter(r -> all((m .- r) .>= 0.),
                            collect(eachrow(table)))'...)
-    display(combs)
     levels = unique(m)
     level_idxs = indexin(levels, m)[2:end]
     push!(level_idxs, nx + 1)
@@ -29,12 +36,29 @@ function partition_press(m::Vector{Int}, k::Int)
     beg = 1
     for l = 1:length(levels)
         stp = level_idxs[l] - 1
-        println("$beg -> $stp")
-        display(combs[:, beg:stp])
-        lvl_perm = map(unique ∘ permutations , eachrow(combs[:, beg:stp]))
-        display(lvl_perm)
+        lvl_perm = map(unique ∘ permutations,
+                            eachrow(combs[:, beg:stp]))
         push!(level_perms, lvl_perm)
         beg = stp + 1
     end
-    collect(product.(zip(level_perms...)))
+    pressed = flatten(product.(level_perms...))
+    collect(map(x -> vcat(x...), pressed))
+end
+
+function partition_push(cs::Vector{Int}, xs::Vector{Int})
+    @assert !isempty(cs)
+    c = first(cs)
+    n = length(cs)
+    base = collect(combinations(xs, c))
+    rst = collect(rest(cs, 2))
+    # exit condition
+    isempty(rst) && return [base]
+    result = collect(Vector{Vector{Int64}}, [])
+    for ys in base
+        rems = setdiff(xs, ys)
+        right = partition_push(rst, rems)
+        # TODO look for speed up here?
+        append!(result, [[ys, r...] for r in right])
+    end
+    result
 end
